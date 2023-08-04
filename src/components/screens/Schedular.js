88f2d1fb49b7c2f47fd10299cbb24d89c5ea2f65
basimsiraj <basimsiraj1@gmail.com>
2023-08-04 10:21:54 +0530

    Assignment container started

--- src/components/screens/Schedular.js
+++ src/components/screens/Schedular.js
@@ -1,15 +1,27 @@
 import React from "react";
 import Header from "../includes/Header";
 import styled from "styled-components";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Schedular() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      speed: 500,
+    };
   return (
     <>
        <Container>
             <Header />
             <LessonContainer>
                 <Heading>New Lessons Board</Heading>
-                <SubjectContainer>
+              
+                <StyledSliderWrapper>
+                <StyledSlider {...settings}>
                     <SubjectListItem>
                         <SubjectImageContainer>
                             <SubjectImage src={require("../../assets/images/Chemistry.jpg")} alt="Chemistry" />
@@ -64,39 +76,143 @@ function Schedular() {
                             </ProfileMainContainer>
                         </ContentContainer>
                     </SubjectListItem>
-                </SubjectContainer>
+                    <SubjectListItem>
+                        <SubjectImageContainer>
+                            <SubjectImage src={require("../../assets/images/Chemistry.jpg")} alt="Chemistry" />
+                        </SubjectImageContainer>
+                        <ContentContainer>
+                            <SubjectTitle>Basic Chemistry III</SubjectTitle>
+                            <SubjectDescription>This part of chemistry deals with atomic structure and molecular structure.</SubjectDescription>
+                            <ProfileMainContainer>
+                                <ProfileImageContainer>
+                                    <Profile src={require("../../assets/images/Women1.png")} alt="Women 1" />
+                                </ProfileImageContainer>
+                                <ProfileSubContainer>
+                                    <PersonName>By Prof Kenni</PersonName>
+                                    <PersonDetails>8:30, 13th March 2020</PersonDetails>
+                                </ProfileSubContainer>
+                            </ProfileMainContainer>
+                        </ContentContainer>
+                    </SubjectListItem>
+                    <SubjectListItem>
+                        <SubjectImageContainer>
+                            <SubjectImage src={require("../../assets/images/Physics.jpg")} alt="Physics" />
+                        </SubjectImageContainer>
+                        <ContentContainer>
+                            <SubjectTitle>Mechanics in Physics</SubjectTitle>
+                            <SubjectDescription>This part of chemistry deals with atomic structure and molecular structure.</SubjectDescription>
+                            <ProfileMainContainer>
+                                <ProfileImageContainer>
+                                    <Profile src={require("../../assets/images/Women2.png")} alt="Women 1" />
+                                </ProfileImageContainer>
+                                <ProfileSubContainer>
+                                    <PersonName>By Prof Kenni</PersonName>
+                                    <PersonDetails>8:30, 13th March 2020</PersonDetails>
+                                </ProfileSubContainer>
+                            </ProfileMainContainer>
+                        </ContentContainer>
+                    </SubjectListItem>
+                    <SubjectListItem>
+                        <SubjectImageContainer>
+                            <SubjectImage src={require("../../assets/images/Focus.jpg")} alt="Focus" />
+                        </SubjectImageContainer>
+                        <ContentContainer>
+                            <SubjectTitle>Focus on Words</SubjectTitle>
+                            <SubjectDescription>This part of chemistry deals with atomic structure and molecular structure.</SubjectDescription>
+                            <ProfileMainContainer>
+                                <ProfileImageContainer>
+                                    <Profile src={require("../../assets/images/Man.png")} alt="Man" />
+                                </ProfileImageContainer>
+                                <ProfileSubContainer>
+                                    <PersonName>By Prof Steven Joe</PersonName>
+                                    <PersonDetails>3 days ago</PersonDetails>
+                                </ProfileSubContainer>
+                            </ProfileMainContainer>
+                        </ContentContainer>
+                    </SubjectListItem>   
+                    </StyledSlider>
+                    </StyledSliderWrapper>
             </LessonContainer>
-       </Container>
-      
+            <BottomContainer>
+                <LeftContainer>
+                    <Title>Assignment Manager</Title>
+                    <AssignmentStatus>
+                        <StatusList>Pending</StatusList>
+                        <StatusList>Completed</StatusList>
+                        <StatusList>Submitted</StatusList>
+                    </AssignmentStatus>
+                    <AssignmentStatusSubContainer>
+                        <AssignmentDetails>
+                            <AssignmentDetailsList>
+                                <SubjectName>Basic Physics III<ProgressBar></ProgressBar>
+                                </SubjectName>
+                                <SubjectDetails>Final assessment | Due 14th March 2020</SubjectDetails>
+                                <ButtonContainer>
+                                    <Button>Resume</Button>
+                                </ButtonContainer>
+                            </AssignmentDetailsList>
+                            <AssignmentDetailsList>
+                                <SubjectName>Mental Math I<ProgressBar></ProgressBar>
+                                </SubjectName>
+                                <SubjectDetails>Class test | Due tomorrow</SubjectDetails>
+                                <ButtonContainer>
+                                    <Button>Start</Button>
+                                </ButtonContainer>
+                            </AssignmentDetailsList>
+                        </AssignmentDetails>     
+                    </AssignmentStatusSubContainer>
+                </LeftContainer>
+                <RightContainer>
+
+                </RightContainer>
+            </BottomContainer>
+       </Container> 
     </>
   );
 }
+
 const Container = styled.div`
-    width: 85%;
-    background-color: gray;
+  width: 85%;
+  background-color: gray;
 `;
+
 const LessonContainer = styled.div`
-    padding-left: 60px;
-    padding-right: 36px;
-    padding-top: 36px;
-   
+  padding-left: 60px;
+  padding-right: 36px;
+  padding-top: 36px;
+  position: relative; 
 `;
+
 const Heading = styled.h2`
-    margin-bottom: 26px;
-    font-size: 19px;
-    font-family: Poppins-Bold;
+  margin-bottom: 26px;
+  font-size: 19px;
+  font-family: Poppins-Bold;
 `;
-const SubjectContainer = styled.ul`
-    display: flex;
+
+const StyledSliderWrapper = styled.div`
+  margin-left: -16px;
+  margin-right: -16px;
+  max-width: 900px; 
+  margin: 0 auto; 
 `;
-const SubjectListItem = styled.li`
-    margin-right: 32px;
-    background-color: white;
-    &:last-child {
-        margin-right: 0;
-    }
-    
+
+const StyledSlider = styled(Slider)`
+  .slick-list {
+    margin: 0 -16px; 
+  }
+
+  .slick-slide {
+    padding: 0 8px; 
+  }
 `;
+
+const SubjectListItem = styled.div`
+  flex: 0 0 calc(33.33% - 16px);
+  margin: 0 8px; 
+  background-color: white;
+`;
+
+
 const SubjectImageContainer = styled.div``;
 const SubjectImage = styled.img`
     display: block;
@@ -136,4 +252,39 @@ const PersonName = styled.span`
 const PersonDetails = styled.p`
     font-size: 10px;
 `;
+const BottomContainer = styled.div`
+    padding-top: 30px;
+    padding-left: 60px;
+    padding-bottom: 40px;
+    padding-right: 0px;
+`;
+const LeftContainer = styled.div`
+    
+`;
+const Title = styled.h6`
+    font-size: 17px;
+    font-family: Poppins-Bold;
+    margin-bottom: 14px;
+`;
+const AssignmentStatus = styled.ul`
+    display: flex;
+`;
+const StatusList = styled.li`
+    margin-right: 32px;
+    font-family: Poppins-Bold;
+    font-size: 13px;
+`;
+const AssignmentStatusSubContainer = styled.div`
+    margin-top: 40px;
+    background-color: white;
+    width: 46%;
+`;
+const AssignmentDetails = styled.ul``;
+const AssignmentDetailsList = styled.li``;
+const SubjectName = styled.h5``;
+const ProgressBar = styled.button``;
+const SubjectDetails = styled.p``;
+const ButtonContainer = styled.div``;
+const Button = styled.button``;
+const RightContainer = styled.div``;
 export default Schedular;
